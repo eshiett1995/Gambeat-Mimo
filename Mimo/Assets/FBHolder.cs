c11fc8f75b37f77345397b33812dfb7880a80899
eshiett1995 <eshiett1995@gmail.com>
2020-01-18 19:45:34 +0100

    pushing facebook login
    pushing facebook login

--- Mimo/Assets/FBHolder.cs
+++ Mimo/Assets/FBHolder.cs
@@ -2,9 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class FBHolder : MonoBehaviour
 {
+
+    public GameObject profilePanel;
+
+    public Text nameText;
+
     private void Awake()
     {
         FB.Init(SetInit, OnHideUnity);
@@ -16,10 +23,10 @@ public class FBHolder : MonoBehaviour
 
         if (FB.IsLoggedIn)
         {
-
+            //profilePanel.SetActive(true);
         }
         else {
-
+            //profilePanel.SetActive(false);
         }
     }
 
@@ -32,23 +39,74 @@ public class FBHolder : MonoBehaviour
         FB.LogInWithReadPermissions(perms, AuthCallback);
     }
 
+   
+
     private void AuthCallback(ILoginResult result)
     {
+
+        Debug.Log("login in result");
+
+        Debug.Log(result);
+
+        //StartCoroutine(Post("https://webhook.site/11470d78-0e31-4dc1-8e06-ea94a6ee5086", JsonUtility.ToJson(result)));
+
         if (FB.IsLoggedIn)
         {
-            // AccessToken class will have session details
             var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
-            // Print current access token's User ID
-            Debug.Log(aToken.UserId);
-            // Print current access token's granted permissions
-            foreach (string perm in aToken.Permissions)
-            {
-                Debug.Log(perm);
-            }
+            FetchFBProfile();
         }
         else
         {
+            StartCoroutine(Post("https://webhook.site/11470d78-0e31-4dc1-8e06-ea94a6ee5086", "{'msg':'error'}"));
             Debug.Log("User cancelled login");
+        }
+    }
+
+    private void FetchFBProfile()
+    {
+        FB.API("/me?fields=first_name,last_name,email", HttpMethod.GET, FetchProfileCallback, new Dictionary<string, string>() { });
+    }
+
+    private void FetchProfileCallback(IGraphResult result)
+    {
+
+        Debug.Log(result.RawResult);
+        StartCoroutine(Post("https://webhook.site/11470d78-0e31-4dc1-8e06-ea94a6ee5086", result.ResultDictionary.ToJson()));
+
+        profilePanel.SetActive(true);
+        nameText.text = "";
+        nameText.text = result.ResultDictionary["first_name"].ToString() +" "+ result.ResultDictionary["last_name"].ToString();
+
+    }
+
+    IEnumerator Post(string url, string bodyJsonString)
+    {
+        var request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(bodyJsonString);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+        Debug.Log("Status Code: " + request.responseCode);
+    }
+
+    IEnumerator Upload(string url, string bodyJsonString)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("myField", "myData");
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
         }
     }
 }
