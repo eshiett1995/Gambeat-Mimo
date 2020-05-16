9b57690a7063fce67a1d85a356be60da2237309d
eshiett1995 <eshiett1995@gmail.com>
2020-05-16 19:41:10 +0100

    pushing paystack
    pushing paystack

---
+++ Mimo/Assets/Utils/Paystack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paystack : MonoBehaviour
+{
+    private AndroidJavaObject javaClass;
+
+    private void Awake()
+    {
+        Debug.Log("in my head");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("carress mem");
+        javaClass = new AndroidJavaObject("com.gambeat.mimo.paystack.paystack.PaystackAndroid");
+        javaClass.CallStatic("initPaystack", "");
+        javaClass.Call("initPayment", "");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
