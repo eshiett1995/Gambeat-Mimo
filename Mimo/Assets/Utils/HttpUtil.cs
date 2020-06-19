b821d385d17d9e513c86d896be3fc64beab0a358
eshiett1995 <eshiett1995@gmail.com>
2020-06-19 02:33:56 +0100

    Update HttpUtil.cs

--- Mimo/Assets/Utils/HttpUtil.cs
+++ Mimo/Assets/Utils/HttpUtil.cs
@@ -5,14 +5,15 @@ using UnityEngine.Networking;
 
 public class HttpUtil
 {
-    static public string baseUrl = "http://35.180.178.43";
+    static public string baseUrl = "http://1fbcc1e9ce2e.ngrok.io"; //http://15.236.202.44
     static public string leaderBoardUrl = baseUrl + "/leader-board";
     static public string userProfileUrl = baseUrl + "/user";
     static public string facebookAuthUrl = baseUrl + "/auth/facebook";
     static public string royalRumbleSearch = baseUrl + "/match/royal-rumble/search";
+    static public string royalRumbleCreate = baseUrl + "/match/royal-rumble/create";
 
 
-    
+
     static public IEnumerator Post(string url, string bodyJsonString, System.Action<UnityWebRequest> callback)
     {
         var request = new UnityWebRequest(url, "POST");
@@ -20,8 +21,8 @@ public class HttpUtil
         request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
         request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
         request.SetRequestHeader("Content-Type", "application/json");
-        //request.SetRequestHeader("Authorization", "eyJhbGciOiJIUzI1NiJ9.eyJwcm92aWRlcl9jcmVkZW50aWFsIjp7ImZpcnN0TmFtZSI6Im90byIsImxhc3ROYW1lIjoiZXNoaWV0dCIsImVtYWlsIjoiZXNoaWV0dDE5OTVAZ21haWwuY29tIiwiaWQiOiIxMjM0NTYifSwicHJvdmlkZXIiOiJmYWNlYm9vayIsImVtYWlsIjoiZXNoaWV0dDE5OTVAZ21haWwuY29tIiwiaXNzIjoiR2FtYmVhdCIsInN1YiI6IkF1dGgifQ.CwspXgmggnt4Eujn0bCYOFmLu9V6KDzU41qLcPKIsyg");
-        request.SetRequestHeader("Authorization", LocalStorageUtil.getAuthKey());
+        request.SetRequestHeader("Authorization", "eyJhbGciOiJIUzI1NiJ9.eyJwcm92aWRlcl9jcmVkZW50aWFsIjp7ImZpcnN0TmFtZSI6Im90byIsImxhc3ROYW1lIjoiZXNoaWV0dCIsImVtYWlsIjoiZXNoaWV0dDE5OTVAZ21haWwuY29tIiwiaWQiOiIxMjM0NTYifSwicHJvdmlkZXIiOiJmYWNlYm9vayIsImVtYWlsIjoiZXNoaWV0dDE5OTVAZ21haWwuY29tIiwiaXNzIjoiR2FtYmVhdCIsInN1YiI6IkF1dGgifQ.CwspXgmggnt4Eujn0bCYOFmLu9V6KDzU41qLcPKIsyg");
+        //request.SetRequestHeader("Authorization", LocalStorageUtil.getAuthKey());
         yield return request.SendWebRequest();
         callback(request);
     }
@@ -33,7 +34,8 @@ public class HttpUtil
         //request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
         request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
         request.SetRequestHeader("Content-Type", "application/json");
-        request.SetRequestHeader("Authorization", LocalStorageUtil.getAuthKey());
+        request.SetRequestHeader("Authorization", "eyJhbGciOiJIUzI1NiJ9.eyJwcm92aWRlcl9jcmVkZW50aWFsIjp7ImZpcnN0TmFtZSI6Im90byIsImxhc3ROYW1lIjoiZXNoaWV0dCIsImVtYWlsIjoiZXNoaWV0dDE5OTVAZ21haWwuY29tIiwiaWQiOiIxMjM0NTYifSwicHJvdmlkZXIiOiJmYWNlYm9vayIsImVtYWlsIjoiZXNoaWV0dDE5OTVAZ21haWwuY29tIiwiaXNzIjoiR2FtYmVhdCIsInN1YiI6IkF1dGgifQ.CwspXgmggnt4Eujn0bCYOFmLu9V6KDzU41qLcPKIsyg");
+        //request.SetRequestHeader("Authorization", LocalStorageUtil.getAuthKey());
         yield return request.SendWebRequest();
         callback(request);
     }
