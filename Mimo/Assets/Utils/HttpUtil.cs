5ea45047e773f1cd7eb7f52d9cfa1f5d9f7f806f
eshiett1995 <eshiett1995@gmail.com>
2020-07-28 15:51:22 +0100

    comited some things

--- Mimo/Assets/Utils/HttpUtil.cs
+++ Mimo/Assets/Utils/HttpUtil.cs
@@ -5,7 +5,7 @@ using UnityEngine.Networking;
 
 public class HttpUtil
 {
-    static public string baseUrl = "https://a40be5123d44.ngrok.io"; //"https://gambeat.com.ng"; //http://15.236.202.44
+    static public string baseUrl = "https://07a1f9e27918.ngrok.io"; //"https://gambeat.com.ng"; //http://15.236.202.44
     static public string leaderBoardUrl = baseUrl + "/leader-board";
     static public string userProfileUrl = baseUrl + "/user";
     static public string facebookAuthUrl = baseUrl + "/auth/facebook";
