0720b93c2db5d73e16d632857c0e6a5cc3e1aa42
eshiett1995 <eshiett1995@gmail.com>
2020-08-06 14:37:49 +0100

    fixed issue with leader board and added mascots
    fixed issue with leader board and added mascots

--- Mimo/Assets/MimoAssets/Scripts/ListViewScript.cs
+++ Mimo/Assets/MimoAssets/Scripts/ListViewScript.cs
@@ -2,22 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static LeaderBoardResponse;
 
 public class ListViewScript : MonoBehaviour
 {
     private int index;
     public Text name, score, indexText;
+    public RawImage avatar;
+    private Mascots mascots;
 
     void Start()
 
     {
+        mascots = FindObjectOfType<Mascots>();
+
         index = FindObjectOfType<GameCode>().leaderboardItems.Count;
-        name.text = Multiplayer.leaderBoardData.ranks[index].firstName;
-        score.text = Multiplayer.lbScores[index] + "";
+        FormattedRank userRank = Multiplayer.leaderBoardData.ranks[index];
+        name.text = $"{userRank.firstName} .{userRank.lastName.Substring(0, 1)}";
+        score.text = userRank.score.ToString();
         int num = index + 1;
         indexText.text = ""+num;
         FindObjectOfType<GameCode>().leaderboardItems.Add(this.gameObject);
-        Debug.Log(Multiplayer.leaderBoardData.ranks[index].photoUrl);
+        if (userRank.photoUrl == null || userRank.photoUrl == "")
+        {
+            SetRandomMascot(avatar, mascots.images);
+        }
+        else {
+            StartCoroutine(FetchProfilePic(avatar, userRank.photoUrl));
+        }
     }
 
+    private IEnumerator FetchProfilePic(RawImage avatar, string url)
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        avatar.texture = www.texture;
+    }
+
+    private void SetRandomMascot(RawImage avatar, List<Sprite> images)
+    {
+        var size = images.Count;
+        Debug.Log("this is the size " + size);
+        avatar.texture = images[Random.Range(0, size)].texture;
+    }
 }
---
+++ Mimo/Assets/MimoAssets/Scripts/Mascots.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mascots : MonoBehaviour
+{
+    public List<Sprite> images;
+}
